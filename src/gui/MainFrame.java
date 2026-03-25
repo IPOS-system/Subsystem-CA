305d63574fc80993bbefd52e9d91801d2dbc0840
humayuncity <humayun.khan@city.ac.uk>
2026-03-25 18:16:02 +0000

    added user class which lets u check the name and role of the user thats logged in

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -61,7 +61,9 @@ public class MainFrame extends JFrame {
 
     public void logout(){
         mainPanel.removeAll();
-        mainPanel.add(new LoginPage(this, loginService), "login");
+        LoginPage loginPage = new LoginPage(this, loginService);
+        mainPanel.add(loginPage, "login");
+        getRootPane().setDefaultButton(loginPage.getLoginBtn()); // makes it so u can press the log in button by pressing enter
         mainPanel.revalidate();
         mainPanel.repaint();
     }
--- src/service/LoginService.java
+++ src/service/LoginService.java
@@ -1,21 +1,30 @@
 package service;
 
+import datastorage.User;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class LoginService {
+    private User LoggedinUser;
 
     public LoginService() {}
-
-
     public boolean authenticate(String entered_username, String entered_password){
         //boolean found_matching_account = false;
         try {
             FileReader fr = new FileReader("src/datastorage/AccountDetails");
             BufferedReader br = new BufferedReader(fr);
             String line;
+            boolean foundstart = false;
             while ((line = br.readLine()) != null) { // go through each account in the database, or each line in the file
+                if (!foundstart) {
+                    if (line.equals("start")) {
+                        foundstart = true;
+                    } else {
+                        continue;
+                    }
+                }
                 String[] parts = line.split(","); // split the string at the commas
 
                 if (parts.length >= 2) {
@@ -23,9 +32,9 @@ public class LoginService {
                     String password_to_check = parts[1]; // second word is the password
 
                     if (username_to_check.equals(entered_username) && password_to_check.equals(entered_password)) { // compare them with the username and password that were typed
-                        //found_matching_account = true; // if theyre the same then a matching account was found
-                        return true;
-                        //break;
+                        String role_of_user = parts[2];
+                        LoggedinUser = new User(entered_username, role_of_user);
+                        return true; // if theyre the same then a matching account was found
                     }
                 }
             }
@@ -36,6 +45,9 @@ public class LoginService {
         }
         //if (!found_matching_account) { return false; } // did not find an account with the given details in the entire file (meaning username or password is incorrect)
         return false;
+    }
+    public User getLoggedinUser() {
+        return LoggedinUser;
     }
 
 }

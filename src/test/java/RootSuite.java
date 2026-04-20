1c84311c05d4ef11440373c50a2bb54d37646574
al453221 <166369408+al453221@users.noreply.github.com>
2026-04-20 22:12:05 +0100

    SaleItem

--- src/test/java/RootSuite.java
+++ src/test/java/RootSuite.java
@@ -6,9 +6,9 @@ import org.junit.runners.Suite;
 // Runs all test classes
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        //SaleItemTest.class,
-        //ItemTest.class,
+        SaleItemTest.class,
         ItemTest.class,
+
 
 
         ISoldStockAPITest.class,
